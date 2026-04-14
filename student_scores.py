d0e143fc71b51053dfbe848e9444f5326bc78113
vallerietran <143464449+vallerietran@users.noreply.github.com>
2026-04-14 18:46:57 -0400

    student_scores.py

--- student_scores.py
+++ student_scores.py
@@ -63,11 +63,82 @@ def pairwise_compatibility_in_project(proj_id):
             # similarity_matrix[f'({userA_id}, {userB_id})'] = user_compatibility_score(userA_id, userB_id)
             similarity_matrix[f'({int(userA_id.split('_')[1])}, {int(userB_id.split('_')[1])})'] = user_compatibility_score(userA_id, userB_id)
 
-# Similarity Matrix
-for i in range(0,5):
+# Compatibility Scores between
+for i in range(0,5): # 5 projects
     proj_id = f'proj_{i}'
-    pairwise_compatibility_in_project(proj_id)
-    print(proj_id)
-    print(similarity_matrix)
+    pairwise_scores = pairwise_compatibility_in_project(proj_id)
+    # print(proj_id)
+    # print(pairwise_scores)
+
+# Similarity Matrix
+def construct_similarity_matrix(users):
+    n = len(users)
+    similarity_matrix = np.zeros((n,n))
+
+    for i in range(0,n):
+        for j in range(0,n):
+            if i < j:
+                score = user_compatibility_score(users[i], users[j])
+                similarity_matrix[i][j] = score
+                similarity_matrix[j][i] = score
+            elif i == j:
+                similarity_matrix[i][j] = 1.0
+    return similarity_matrix
+
+# Clustering students using a Greedy Algorithm + Group Size Balancing
+def cluster_students(users):
+    similarity_matrix = construct_similarity_matrix(users)
+
+    n = len(users)
+    target_group_size = 3
+    k = min(max(1, round(n / target_group_size)), n // 2) if n >= 4 else 1
+
+    # Set of users remaining to assign into groups
+    unassigned_users = set(range(n))
+    groups = []
+
+    # Average Similarity Score of a Group
+    def avg_similarity(i, group):
+        if not group: return 0
+        return np.mean([similarity_matrix[i][j] for j in group])
+    
+    for _ in range(0, k-1):
+        if len(unassigned_users) == 0:
+            break
+        
+        # Choose random seed user to build group around
+        seed_user = max(unassigned_users, key=lambda i: sum(similarity_matrix[i][j] for j in unassigned_users))
+        unassigned_users.remove(seed_user)
+
+        # Adding most compatibile users into group with seed user
+        group = [seed_user]
+        while len(group) < target_group_size and len(group) < len(unassigned_users):
+            most_compatible_user = max(unassigned_users, key=lambda j: avg_similarity(j,group))
+            group.append(most_compatible_user)
+            unassigned_users.remove(most_compatible_user)
+        groups.append(group)
+
+    # Last group gets remaining users
+    if len(unassigned_users) != 0:
+        groups.append(list(unassigned_users))
+
+    # Add singletons to existing groups so that there are no single groups
+    new_groups = []
+    singletons = []
+    for group in groups:
+        if len(group) == 1:
+            singletons.append(group[0])
+        else:
+            new_groups.append(group)
+    for singleton in singletons:
+            most_compatible_group = max(groups, key=lambda i: np.mean([similarity_matrix[singleton][j] for j in i]))
+            most_compatible_group.append(singleton)
+    return [[users[i] for i in group if group] for group in new_groups]
 
 
+for proj_id, users in project_scores.proj_assignments.items():
+    groups = cluster_students(users)
+
+    print(f'\n{proj_id}:')
+    for group_id, members in enumerate(groups):
+        print(f'Group {group_id}: {members}' )
